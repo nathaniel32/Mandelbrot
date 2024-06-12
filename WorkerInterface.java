e3f8f7b15b54c755b08491149c3e1be2fb55b119
nathaniel32 <nathaniel_sigit@yahoo.com>
2024-06-12 19:09:47 +0200

    remove bug

--- WorkerInterface.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface WorkerInterface extends Remote {
-    int[][] bild_rechnen_worker(int workers_threads, int max_iter, double max_betrag, int y_sta, int y_sto, int xpix, int ypix, double xmin, double xmax, double ymin, double ymax) throws RemoteException;
-    boolean worker_status() throws RemoteException;
-    void worker_buchen() throws RemoteException;
-}
